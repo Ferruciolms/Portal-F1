82e85d69c14f379a2a81681b9638ada81003ccae
Ferruciolms <145029119+Ferruciolms@users.noreply.github.com>
2024-08-13 18:42:53 -0300

    #0017 - Circuit filter funcionando

--- portal_f1/applications/analytics/views/circuit.py
+++ portal_f1/applications/analytics/views/circuit.py
@@ -1,9 +1,26 @@
 from django.views.generic import ListView
+from django.db.models import FloatField, Value, IntegerField, Min, CharField
+from analytics.filters.circuit import CircuitFilter
 from analytics.models.circuits import Circuit
+from analytics.models.results import Result
 
 
 class CircuitList(ListView):
     model = Circuit
     template_name = 'circuit/circuit_list.html'
     paginate_by = 10
-    # permission_required = 'analytics.view_circuit'
+    permission_required = 'analytics.view_circuit'
+
+    def dispatch(self, request, *args, **kwargs):
+        return super(CircuitList, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = CircuitFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = CircuitFilter(self.request.GET, queryset=queryset).qs
+
+        return data
